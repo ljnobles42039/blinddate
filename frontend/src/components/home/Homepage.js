bfb8494842bcde742387b5af1b60cee0b8ce909f
Larry Nobles <larrynobles@Larrys-MacBook-Pro.local>
2019-10-09 18:40:55 -0500

    before deplay

--- frontend/src/components/home/Homepage.js
+++ frontend/src/components/home/Homepage.js
@@ -11,7 +11,7 @@ export default class Homepage extends Component {
       <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', width: '100vw', height: '100vh'   }}>
         <div style={{ width: '80vw', height: '70vh',  backgroundImage: 'url("/background.jpeg")', backgroundSize: 'cover'  }}>
           <div style={{marginTop: '5vh',marginLeft: '27vw', display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', width: '35vw', alignContent: 'center'}}>
-            <p style={{fontSize: '3rem', marginLeft:'6vw'}}>Blind Date</p>
+            <p style={{fontSize: '3rem', mAarginLeft:'6vw'}}>Blind Date</p>
             <p style={{fontSize: '2rem', color: "white"}} >Take a chance with interesting experiences in your city</p>
             <Link to="/signup"> <Button type="submit" value="signup" style={{ width: '30vw', marginRight: '10vw', marginTop: '6vh'}}>Sign up</Button></Link>
             <Link to="/login"> <Button type="submit" value="Login" style={{ width: '30vw', marginRight: '10vw', marginTop: '6vh'}}>Login</Button></Link>
